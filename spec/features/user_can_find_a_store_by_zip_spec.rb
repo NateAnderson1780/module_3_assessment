3a3b3e9dd4d64e13e35cb2dd25449f44a0438357
Nate Anderson <curtislowe10@gmail.com>
2016-11-02 10:03:42 -0600

    1003 commit

---
+++ spec/features/user_can_find_a_store_by_zip_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "user can find stores by zip code" do
+  it "can search stores by zip code" do
+    visit '/'
+    save_and_open_page
+
+    fill_in "Find Stores by Zip Code", with: 80202
+    click_on "Search"
+
+    expect(current_path).to eq('/search')
+
+    expect(page).to have_content('17 total stores')
+
+    15.times do |i|
+      within(".stores") do
+        expect(page).to have_content('Name:')
+        expect(page).to have_content('City:')
+        expect(page).to have_content('Distance:')
+        expect(page).to have_content('Store Type:')
+      end
+    end
+  end
+end
