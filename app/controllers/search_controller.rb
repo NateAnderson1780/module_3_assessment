109500f44f40a2189adc46ef789906ff545aa914
Nate Anderson <curtislowe10@gmail.com>
2016-11-02 10:48:43 -0600

    1048 commit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
     @stores = Store.by_zip_code(params[:zip_code])
+    byebug
   end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -3,9 +3,32 @@ class Store
     @data = raw_store_params
   end
 
+  def long_name
+
+  end
+
+  def city
+
+  end
+
+  def distance
+
+  end
+
+  def phone_number
+
+  end
+
+  def store_type
+
+  end
+
   def self.by_zip_code(zip_code)
     BestBuyService.find_by_zip_code(zip_code).map do |raw_store|
       Store.new(raw_store)
     end
   end
+
+  private
+    attr_reader :data
 end
